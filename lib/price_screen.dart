63b8c2e419d915ccc87690dddaf25490f5ff910d
divyesh <skdivyesh333@gmail.com>
2020-07-04 14:42:42 +0530

    modify api call

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'coin_data.dart';
@@ -12,8 +11,6 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
-  String selectedCurrency = 'USD';
-
   List<DropdownMenuItem> dropDownButtonListItem() {
     List<DropdownMenuItem<String>> dropDownItems = [];
     for (var currency in currenciesList) {
@@ -33,6 +30,9 @@ class _PriceScreenState extends State<PriceScreen> {
       onChanged: (value) {
         setState(() {
           selectedCurrency = value;
+          for (String cryptoCoin in cryptoList) {
+            getCurrencyPrice(cryptoCoin);
+          }
         });
       },
     );
@@ -57,22 +57,36 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 
-  var convertedPrice;
-  String url =
-      'https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=8C3EB7D6-49A8-4A6F-A664-11E3D631E369';
+  String selectedCurrency = 'USD';
+  String bitCoin = 'BTC';
+  String usdPrice;
+  String apiKey = '8C3EB7D6-49A8-4A6F-A664-11E3D631E369';
+  String url = 'https://rest.coinapi.io/v1/exchangerate/';
 
-  void getCurrentPrice() async {
-    http.Response response = await http.get(url);
-    print(response.statusCode);
-    var decodedData = jsonDecode(response.body);
+  Future getCurrencyPrice(String cryptoCoin) async {
+    bitCoin = cryptoCoin;
+    http.Response response =
+        await http.get('$url$bitCoin/$selectedCurrency?apikey=$apiKey');
+    print(response.statusCode); //to confirm if api is working
+
+    dynamic decodedData = jsonDecode(response.body);
     print(decodedData);
-    var price = decodedData['rate'];
-    convertedPrice = price.toStringAsFixed(2);
+    setState(() {
+      usdPrice = decodedData['rate'].toStringAsFixed(2);
+    });
+    print(usdPrice);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    for (String cryptoCoins in cryptoList) {
+      getCurrencyPrice(cryptoCoins);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    getCurrentPrice();
     return Scaffold(
       appBar: AppBar(
         title: Text('🤑 Coin Ticker'),
@@ -92,7 +106,49 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = $convertedPrice USD',
+                  '1 $bitCoin = $usdPrice $selectedCurrency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 $bitCoin = $usdPrice $selectedCurrency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 $bitCoin = $usdPrice $selectedCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
